f68043b6c3ec76fd059c3acb76c4333a73a9c0d0
germanpachec0 <germanpacheco98@gmail.com>
2026-04-19 15:22:57 -0300

    completed 04-database/01-add-migrations

--- project/backend/orders/adapters/db/migrations/0001_init_orders.up.sql
+++ project/backend/orders/adapters/db/migrations/0001_init_orders.up.sql
@@ -1,1 +1,12 @@
 -- todo: implement
+
+BEGIN;
+CREATE SCHEMA IF NOT EXISTS orders;
+CREATE TABLE IF NOT EXISTS orders.customers (
+    customer_uuid UUID NOT NULL PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    address JSONB NOT NULL,
+    phone_number varchar(50) NOT NULL
+);
+COMMIT;
