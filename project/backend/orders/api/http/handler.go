7f7451733a4b802dc5705afa7222847aad3d07aa
germanpachec0 <germanpacheco98@gmail.com>
2026-04-19 18:19:17 -0300

    completed 04-database/03-insert-customer

--- project/backend/orders/api/http/handler.go
+++ project/backend/orders/api/http/handler.go
@@ -4,17 +4,53 @@ import (
 	"context"
 
 	"eats/backend/common"
+	"eats/backend/common/shared"
+	"eats/backend/orders/adapters/db/dbmodels"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type Handler struct{}
+type Handler struct {
+	db *pgxpool.Pool
+}
 
-func NewHandler() Handler {
-	return Handler{}
+func NewHandler(db *pgxpool.Pool) Handler {
+	return Handler{db: db}
 }
 
 func (h Handler) RegisterCustomer(ctx context.Context, request RegisterCustomerRequestObject) (RegisterCustomerResponseObject, error) {
 	customerUUID := common.NewUUIDv7()
+	query := dbmodels.New(h.db)
+	address, err := shared.NewAddress(
+		request.Body.Address.Line1,
+		request.Body.Address.Line2,
+		request.Body.Address.PostalCode,
+		request.Body.Address.City,
+		request.Body.Address.CountryCode,
+	)
+	if err != nil {
+		return RegisterCustomer400JSONResponse{
+			BadRequestJSONResponse: BadRequestJSONResponse{
+				Message: "failed to create address",
+			},
+		}, nil
+	}
+	email := string(request.Body.Email)
 
+	err = query.InsertCustomer(ctx, dbmodels.InsertCustomerParams{
+		CustomerUuid: customerUUID,
+		Name:         request.Body.Name,
+		Email:        email,
+		Address:      address,
+		PhoneNumber:  request.Body.PhoneNumber,
+	})
+	if err != nil {
+		return RegisterCustomer400JSONResponse{
+			BadRequestJSONResponse: BadRequestJSONResponse{
+				Message: "failed to register customer",
+			},
+		}, nil
+	}
 	return RegisterCustomer201JSONResponse{
 		CustomerUuid: customerUUID,
 	}, nil
