551e0a56ad1fc60c804919a6cf1ddd077283f38f
germanpachec0 <germanpacheco98@gmail.com>
2026-04-19 18:05:52 -0300

    completed 04-database/02-generate-sqlc

--- project/backend/orders/adapters/db/queries/customers.sql
+++ project/backend/orders/adapters/db/queries/customers.sql
@@ -1,1 +1,10 @@
--- todo: implement
+
+-- name: InsertCustomer :exec
+INSERT INTO orders.customers (customer_uuid, name, email, address, phone_number)
+VALUES ($1, $2, $3, $4, $5)
+RETURNING customer_uuid, name, email, address, phone_number;
+
+-- name: GetCustomerByUUID :one
+SELECT customer_uuid, name, email, address, phone_number
+FROM orders.customers
+WHERE customer_uuid = $1;
