510f73c8da1c51e966db478b2d6e07ae5a4b1422
germanpachec0 <germanpacheco98@gmail.com>
2026-04-20 21:13:51 -0300

    completed 06-application-layer/01-app-types

--- project/backend/orders/adapters/db/queries/customers.sql
+++ project/backend/orders/adapters/db/queries/customers.sql
@@ -1,1 +1,21 @@
--- todo: implement
+
+-- name: InsertCustomer :exec
+INSERT INTO orders.customers (
+	customer_uuid,
+	name,
+	email,
+	address,
+	phone_number
+) VALUES (
+	$1, $2, $3, $4, $5
+);
+
+-- name: GetCustomerByUUID :one
+SELECT
+	customer_uuid,
+	name,
+	email,
+	address,
+	phone_number
+FROM orders.customers
+WHERE customer_uuid = $1;
--- project/backend/orders/api/http/handler.go
+++ project/backend/orders/api/http/handler.go
@@ -4,14 +4,63 @@ import (
 	"context"
 
 	"eats/backend/common"
+	"eats/backend/common/shared"
+	"eats/backend/orders/app"
 )
 
-type Handler struct{}
-
-func NewHandler() Handler {
-	return Handler{}
+type Handler struct {
+	svc *app.Service
 }
 
-func Register(ctx context.Context, e common.EchoRouter, handler Handler) error {
+func NewHandler(
+	svc *app.Service,
+) Handler {
+	if svc == nil {
+		panic("svc cannot be nil")
+	}
+
+	return Handler{
+		svc: svc,
+	}
+}
+
+func (h Handler) RegisterCustomer(ctx context.Context, request RegisterCustomerRequestObject) (RegisterCustomerResponseObject, error) {
+	customerUUID := common.NewUUIDv7()
+
+	address, err := openapiAddressToSharedAddress(request.Body.Address)
+	if err != nil {
+		return nil, err
+	}
+	customer := app.Customer{
+		CustomerUUID: customerUUID,
+		Name:         request.Body.Name,
+		Email:        string(request.Body.Email),
+		Address:      address,
+		PhoneNumber:  request.Body.PhoneNumber,
+	}
+
+	err = h.svc.RegisterCustomer(ctx, customer)
+	if err != nil {
+		return nil, err
+	}
+
+	return RegisterCustomer201JSONResponse{
+		CustomerUuid: customerUUID,
+	}, nil
+}
+
+func Register(ctx context.Context, e EchoRouter, handler Handler) error {
+	RegisterHandlers(e, NewStrictHandler(handler, nil))
+
 	return nil
+}
+
+func openapiAddressToSharedAddress(addr Address) (shared.Address, error) {
+	return shared.Address{
+		City:        addr.City,
+		CountryCode: shared.CountryCode(addr.CountryCode),
+		Line1:       addr.Line1,
+		Line2:       addr.Line2,
+		PostalCode:  addr.PostalCode,
+	}, nil
 }
